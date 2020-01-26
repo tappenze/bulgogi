49ef9fefe371b4ef4f26881fa47169f3745fc918
Tom Appenzeller <tappenze@purdue.edu>
2020-01-25 23:54:48 -0500

    all searches completed

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,26 @@ class App extends Component {
         artist3: "",
         name4: "",
         artist4: ""
+      },
+      albums: {
+        album1: "",
+        artist1: "",
+        album2: "",
+        artist2: "",
+        album3: "",
+        artist3: "",
+        album4: "",
+        artist4: ""
+      },
+      playlists: {
+        playlist1: "",
+        creator1: "",
+        playlist2: "",
+        creator2: "",
+        playlist3: "",
+        creator3: "",
+        playlist4: "",
+        creator4: ""
       }
     };
   }
@@ -80,6 +100,40 @@ class App extends Component {
     });
   }
 
+  getAlbums(album_name) {
+    spotifyApi.searchAlbums(album_name).then(response => {
+      this.setState({
+        albums: {
+          album1: response.albums.items[0].name,
+          artist1: response.albums.items[0].artists[0].name,
+          album2: response.albums.items[1].name,
+          artist2: response.albums.items[1].artists[0].name,
+          album3: response.albums.items[2].name,
+          artist3: response.albums.items[2].artists[0].name,
+          album4: response.albums.items[3].name,
+          artist4: response.albums.items[3].artists[0].name
+        }
+      });
+    });
+  }
+
+  getPlaylists(playlist_name) {
+    spotifyApi.searchPlaylists(playlist_name).then(response => {
+      this.setState({
+        playlists: {
+          playlist1: response.playlists.items[0].name,
+          creator1: response.playlists.items[0].owner.display_name,
+          playlist2: response.playlists.items[1].name,
+          creator2: response.playlists.items[1].owner.display_name,
+          playlist3: response.playlists.items[2].name,
+          creator3: response.playlists.items[2].owner.display_name,
+          playlist4: response.playlists.items[3].name,
+          creator4: response.playlists.items[3].owner.display_name
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -96,6 +150,37 @@ class App extends Component {
         <div>
           Song 4: {this.state.songs.name4} By {this.state.songs.artist4}
         </div>
+
+        <div>
+          Album 1: {this.state.albums.album1} By {this.state.albums.artist1}
+        </div>
+        <div>
+          Album 2: {this.state.albums.album2} By {this.state.albums.artist2}
+        </div>
+        <div>
+          Album 3: {this.state.albums.album3} By {this.state.albums.artist3}
+        </div>
+        <div>
+          Album 4: {this.state.albums.album4} By {this.state.albums.artist4}
+        </div>
+
+        <div>
+          Playlist 1: {this.state.playlists.playlist1} By{" "}
+          {this.state.playlists.creator1}
+        </div>
+        <div>
+          Playlist 2: {this.state.playlists.playlist2} By{" "}
+          {this.state.playlists.creator2}
+        </div>
+        <div>
+          Playlist 3: {this.state.playlists.playlist3} By{" "}
+          {this.state.playlists.creator3}
+        </div>
+        <div>
+          Playlist 4: {this.state.playlists.playlist4} By{" "}
+          {this.state.playlists.creator4}
+        </div>
+
         <div>Artist searched for: {this.state.artist.name}</div>
         <div>
           <img src={this.state.artist.face} style={{ height: 150 }} />
@@ -116,6 +201,14 @@ class App extends Component {
         )}
         {this.state.loggedIn && (
           <button onClick={() => this.getSong("Hero")}>Get Songs</button>
+        )}
+        {this.state.loggedIn && (
+          <button onClick={() => this.getAlbums("Stoney")}>Get Albums</button>
+        )}
+        {this.state.loggedIn && (
+          <button onClick={() => this.getPlaylists("Fight")}>
+            Get Playlists
+          </button>
         )}
       </div>
     );
