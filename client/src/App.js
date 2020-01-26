189ab9bf619978f727df4c9a485bd34265140964
Tom Appenzeller <tappenze@purdue.edu>
2020-01-25 23:05:29 -0500

    Music search functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import SpotifyWebApi from 'spotify-web-api-js';
+import SpotifyWebApi from "spotify-web-api-js";
 const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     const params = this.getHashParams();
     const token = params.access_token;
@@ -14,47 +14,109 @@ class App extends Component {
     }
     this.state = {
       loggedIn: token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: '' }
-    }
+      nowPlaying: { name: "Not Checked", albumArt: "" },
+      artist: { name: "Verify", face: "" },
+      songs: {
+        name1: "",
+        artist1: "",
+        name2: "",
+        artist2: "",
+        name3: "",
+        artist3: "",
+        name4: "",
+        artist4: ""
+      }
+    };
   }
   getHashParams() {
     var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
     while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
     }
     return hashParams;
   }
 
-  getNowPlaying(){
-    spotifyApi.getMyCurrentPlaybackState()
-      .then((response) => {
-        this.setState({
-          nowPlaying: { 
-              name: response.item.name, 
-              albumArt: response.item.album.images[0].url
-            }
-        });
-      })
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then(response => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url
+        }
+      });
+    });
   }
+
+  getSongByArtist(artist_name) {
+    spotifyApi.searchArtists(artist_name).then(response => {
+      this.setState({
+        artist: {
+          name: response.artists.items[0].name,
+          face: response.artists.items[0].images[0].url
+        }
+      });
+    });
+  }
+
+  getSong(song_name) {
+    spotifyApi.searchTracks(song_name).then(response => {
+      this.setState({
+        songs: {
+          name1: response.tracks.items[0].name,
+          artist1: response.tracks.items[0].artists[0].name,
+          name2: response.tracks.items[1].name,
+          artist2: response.tracks.items[1].artists[0].name,
+          name3: response.tracks.items[2].name,
+          artist3: response.tracks.items[2].artists[0].name,
+          name4: response.tracks.items[3].name,
+          artist4: response.tracks.items[3].artists[0].name
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <a href='http://localhost:8888' > Login to Spotify </a>
+        <a href="http://localhost:8888"> Login to Spotify </a>
         <div>
-          Now Playing: { this.state.nowPlaying.name }
+          Song 1: {this.state.songs.name1} By {this.state.songs.artist1}
         </div>
         <div>
-          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
+          Song 2: {this.state.songs.name2} By {this.state.songs.artist2}
         </div>
-        { this.state.loggedIn &&
+        <div>
+          Song 3: {this.state.songs.name3} By {this.state.songs.artist3}
+        </div>
+        <div>
+          Song 4: {this.state.songs.name4} By {this.state.songs.artist4}
+        </div>
+        <div>Artist searched for: {this.state.artist.name}</div>
+        <div>
+          <img src={this.state.artist.face} style={{ height: 150 }} />
+        </div>
+        <div>Now Playing: {this.state.nowPlaying.name}</div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+        </div>
+        {this.state.loggedIn && (
           <button onClick={() => this.getNowPlaying()}>
             Check Now Playing
           </button>
-        }
+        )}
+        {this.state.loggedIn && (
+          <button onClick={() => this.getSongByArtist("Cher")}>
+            Get artist Cher
+          </button>
+        )}
+        {this.state.loggedIn && (
+          <button onClick={() => this.getSong("Hero")}>Get Songs</button>
+        )}
       </div>
     );
   }
